75c3b68d3a119546b87a3cce58014619f3c085dc
Adriaan de Beer <adriaan@spaceadvisory.com>
2020-09-25 09:57:15 +0000

    Broken test

--- post-deployment/tests/test_tango-cpp.py
+++ post-deployment/tests/test_tango-cpp.py
@@ -12,20 +12,25 @@ def test_call_command_and_test_attribute():
     """Call Command and test attribute."""
 
 
-@given(parsers.parse('a device called <device_name>'))
+@given(parsers.parse('a device called {device_name}'))
 def device_proxy(run_context, device_name):
     """a device called sys/tg_test/1."""
     return tango.DeviceProxy(device_name)
 
 
 @pytest.fixture
-@when(parsers.parse('I call the command <command_name>'))
+@when(parsers.parse('I call the command {command_name}'))
 def call_command(device_proxy,command_name):
     """I call the command State()."""
+    logging.info(device_proxy)
+    logging.info("command name: {}".format(command_name))
+    logging.info("BEFORE COMMAND CALLED")
+    logging.info(device_proxy.command_inout(command_name))
+    logging.info("AFTER COMMAND CALLED")
     return device_proxy.command_inout(command_name)
 
 
-@then(parsers.parse('the attribute <attribute_name} is <expected_value>'))
+@then(parsers.parse('the attribute {attribute_name} is {expected_value}'))
 def check_attribute(device_proxy, attribute_name, expected_value):
     """the attribute State is RUNNING."""
     attr = device_proxy.read_attribute(attribute_name)
