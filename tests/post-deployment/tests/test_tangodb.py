fc6f2f6b0d142f565aba0b44c69feededbd10026
Katleho Madisa <katleho.madisa47@gmail.com>
2021-10-21 08:58:45 +0200

    ST-976 Refactored the tests to make use of a mysql_cursor fixture Co-authored-by: Abhijeet Deolalikar <abhijeet_deolalikar@persistent.com>

--- tests/post-deployment/tests/test_tangodb.py
+++ tests/post-deployment/tests/test_tangodb.py
@@ -4,128 +4,104 @@ import yaml
 import mysql.connector
 
 FILE_PATH = "/app/testing/tango_values.yaml"
-mysql_cnx = None
 
 
-@pytest.fixture
-def getValuesFile():
+def get_values_file():
+
     try:
         with open(FILE_PATH) as file:
-            values_list = yaml.load(file, Loader=yaml.FullLoader)
-            return values_list
+            pytest.values_list = yaml.load(file, Loader=yaml.FullLoader)
+    except Exception as e:
+        pytest.raises(e)
+
+
+def get_db_config():
+    try:
+        pytest.dbHost = "ska-tango-base-" + pytest.values_list["tangodb"]["component"]
+        pytest.dbName = pytest.values_list["tangodb"]["db"]["db"]
+        pytest.dbUser = pytest.values_list["tangodb"]["db"]["user"]
+        pytest.dbPassword = pytest.values_list["tangodb"]["db"]["password"]
     except Exception as e:
         pytest.raises(e)
 
 
 @pytest.fixture
-def getDBConfig(getValuesFile):
-    mysql_config = dict()
+def mysql_cursor():
+    get_values_file()
+    get_db_config()
+    mysql_cnx = None
     try:
-        mysql_config["host"] = 'ska-tango-base-' + getValuesFile['tangodb']['component']
-        mysql_config["database"] = getValuesFile['tangodb']['db']['db']
-        mysql_config["user"] = getValuesFile['tangodb']['db']['user']
-        mysql_config["password"] = getValuesFile['tangodb']['db']['password']
-        return mysql_config
-    except Exception as e:
-        pytest.raises(e)
-
-
-def init_mysql_db_connection(getDBConfig):
-    global mysql_cnx
-    try:
-        logging.debug(
-            "########################### Initializing Mysql Connection " + str(mysql_cnx))
-        mysql_cnx = mysql.connector.connect(**getDBConfig)
+        mysql_cnx = mysql.connector.connect(
+            user=pytest.dbUser,
+            password=pytest.dbPassword,
+            host=pytest.dbHost,
+            database=pytest.dbName,
+        )
     except mysql.connector.Error as err:
         logging.error(err)
         pytest.raises(Exception("Database connection Error"))
 
-
-def delete_mysql_db_connection():
-    logging.debug(
-        "########################### Closing Mysql Connection " + str(mysql_cnx))
+    cursor = mysql_cnx.cursor()
+    yield cursor
+    cursor.close()
     mysql_cnx.close()
 
 
-def test_database_connection(getDBConfig):
-    try:
-        cnx = mysql.connector.connect(**getDBConfig)
-
-    except mysql.connector.Error as err:
-        logging.error(err)
-        pytest.raises(Exception("Database connection Error"))
-    else:
-        cnx.close()
-        assert 1
-
-
-def test_tango_database_is_present():
-    cursor = mysql_cnx.cursor()
-    try:
-        cursor.execute("SHOW DATABASES")
-    except mysql.connector.Error as err:
-        print("Failed creating database: {}".format(err))
-        cursor.close()
-        delete_mysql_db_connection()
-        exit(1)
+def test_tango_database_is_present(mysql_cursor):
+    mysql_cursor.execute("SHOW DATABASES")
 
     database_names = []
-    for database_name in cursor:  # NB database_name is a tuple
-        print(database_name)
-        database_names.append(database_name)
+    for database_name in mysql_cursor:
+        database_names.append(database_name[0])
 
-    cursor.close()
-    delete_mysql_db_connection()
-    assert ('tango',) in database_names
+    assert "tango" in database_names
 
 
-def test_tango_database_tables():
-    cursor = mysql_cnx.cursor()
-    try:
-        cursor.execute("SHOW TABLES")
-    except mysql.connector.Error as err:
-        print("Failed creating database: {}".format(err))
-        cursor.close()
-        delete_mysql_db_connection()
-        exit(1)
+def test_tango_database_tables(mysql_cursor):
+    mysql_cursor.execute("SHOW TABLES")
+
     expected_tables = (
-        'access_address', 'access_device', 'attribute_alias', 'attribute_class',
-        'class_attribute_history_id', 'class_history_id', 'class_pipe_history_id',
-        'device', 'device_attribute_history_id', 'device_history_id',
-        'device_pipe_history_id', 'event', 'object_history_id', 'property',
-        'property_attribute_class', 'property_attribute_class_hist',
-        'property_attribute_device', 'property_attribute_device_hist',
-        'property_class', 'property_class_hist', 'property_device',
-        'property_device_hist', 'property_hist', 'property_pipe_class',
-        'property_pipe_class_hist', 'property_pipe_device',
-        'property_pipe_device_hist', 'server'
+        "access_address",
+        "access_device",
+        "attribute_alias",
+        "attribute_class",
+        "class_attribute_history_id",
+        "class_history_id",
+        "class_pipe_history_id",
+        "device",
+        "device_attribute_history_id",
+        "device_history_id",
+        "device_pipe_history_id",
+        "event",
+        "object_history_id",
+        "property",
+        "property_attribute_class",
+        "property_attribute_class_hist",
+        "property_attribute_device",
+        "property_attribute_device_hist",
+        "property_class",
+        "property_class_hist",
+        "property_device",
+        "property_device_hist",
+        "property_hist",
+        "property_pipe_class",
+        "property_pipe_class_hist",
+        "property_pipe_device",
+        "property_pipe_device_hist",
+        "server",
     )
     actual_tables = []
-    for table_name in cursor:  # NB table_name is a tuple
-        print(table_name)
+    for table_name in mysql_cursor:
         actual_tables.append(table_name[0])
 
-    cursor.close()
-    delete_mysql_db_connection()
     assert actual_tables == list(expected_tables)
 
 
-def test_databaseds_server_is_registered_in_the_database():
-    cursor = mysql_cnx.cursor()
-
-    try:
-        cursor.execute("SELECT name FROM device")
-    except mysql.connector.Error as err:
-        print("Failed creating database: {}".format(err))
-        cursor.close()
-        delete_mysql_db_connection()
-        exit(1)
+def test_databaseds_server_is_registered_in_the_database(mysql_cursor):
+    mysql_cursor.execute("SELECT name FROM device")
 
     actual_devices = []
-    for device_name in cursor:  # NB device_name is a tuple
-        print(device_name)
+    for device_name in mysql_cursor:
         actual_devices.append(device_name[0])
 
-    cursor.close()
-    delete_mysql_db_connection()
     assert "sys/database/2" in actual_devices
