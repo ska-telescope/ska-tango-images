7baca90f2914e2e30eb07b428750981faae50833
Katleho Madisa <katleho.madisa47@gmail.com>
2021-10-29 12:50:56 +0200

    ST-976 Removed the unused run_context Co-authored-by: Abhijeet Deolalikar <abhijeet_deolalikar@persistent.com>

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -4,27 +4,6 @@ import pytest
 import yaml
 import mysql.connector
 
-from tango import DeviceProxy
-from collections import namedtuple
-
-## shared fixtures
-from resources.test_support.fixtures import run_context
-
-"""
-Taken from https://gitlab.com/ska-telescope/skampi/-/blob/21e00f36a5fa797ad816ec8c40954f6a3a70b040/post-deployment/tests/conftest.py
-
-RunContext is a metadata object to access values from the environment, 
-i.e. data that is injected in by the Makefile. Useful if tests need to 
-be aware of the k8s context they are running in, such as the HELM_RELEASE.
-
-This will allow tests to resolve hostnames and other dynamic properties.
-
-Example:
-
-def test_something(run_context):
-    HOSTNAME = 'some-pod-{}'.format(run_context.HELM_RELEASE)
-
-"""
 
 FILE_PATH = os.path.abspath("tests/tango_values.yaml")
 
